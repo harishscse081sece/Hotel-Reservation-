9e4addecf3ccfcf4c6e2b09b434a2870dcb0738c
harishscse081sece <harish.s2024cse@sece.ac.in>
2025-09-16 23:09:52 +0530

    Added customer class

---
+++ code.java
@@ -0,0 +1,19 @@
+import.java.util.*;
+// Customer class
+class Customer {
+    private int customerId;
+    private String name;
+    private String phone;
+    private String email;
+
+    public Customer(int customerId, String name, String phone, String email) {
+        this.customerId = customerId;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
